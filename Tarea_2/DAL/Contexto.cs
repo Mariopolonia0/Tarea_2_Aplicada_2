33f15b07526440c566d83cc6834d3b9564cb9b87
Mario <Mario@DESKTOP-MARIO>
2021-09-12 21:44:55 -0400

    Add project files.

---
+++ Tarea_2/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea_2.BLL;
+
+namespace Tarea_2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+    }
+}
---
+++ Tarea_2/Models/Personas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea_2.BLL
+{
+    public class Personas
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="No Ha Digitado El Nombre")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "No Ha Digitado El Telefono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "No Ha Digitado La Cedula")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "No Ha Digitado La Dirrecion")]
+        public string Dirrecion { get; set; }
+
+        [Required(ErrorMessage = "No Ha Digitado Fecha de Naciminto")]
+        public DateTime FechaNacimiento { get; set; }
+
+        //telefono,cedula,dirrecion,fechanaciominto
+    }
+}
